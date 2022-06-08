9aba9ba2d19eefdac495e9f11e9dbe18a9e87f19
Mohammad <zia.ro@live.com>
2022-06-08 00:02:29 -0400

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function focusFo(){
+        console.log('Good!')
+    }
+    function blurFo(){
+        console.log('Hey! Eyes on me!')
+    }
+    return <button onFocus={focusFo} onBlur={blurFo}>Eyes on me</button>
+}
+
+export default EyesOnMe
+
+
+
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+function Keypad(){
+    function eventHandler(){
+        console.log('Entering password...')
+    }
+
+    return <div>
+        <input type="password" onChange={eventHandler}></input>
+    </div>
+}
+
+export default Keypad
